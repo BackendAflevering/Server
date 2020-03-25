2445335574dbeb97cb96d05753230618d5173344
jensc <jenschrj97@gmail.com>
2020-03-25 11:14:42 +0100

    Lavede login til brugerautorisation ogstart på RESTcontroller så den kan tjekke et login

--- src/main/java/controller/Rest_controller.java
+++ src/main/java/controller/Rest_controller.java
@@ -1,4 +1,38 @@
 package controller;
 
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import logik.Login;
+import org.jetbrains.annotations.NotNull;
+
+import java.net.MalformedURLException;
+
+
 public class Rest_controller {
+   public static Javalin server;
+   public static Login login;
+   public void stop(){
+       server.stop();
+       server = null;
+   }
+
+   public void start() throws Exception{
+       if (server!=null) return;
+
+       server = Javalin.create().start(4548);
+       server.exception(Exception.class, (e,ctx)-> {e.printStackTrace();});
+       //TODO: lav endpoints (GET og POST)
+
+   }
+
+   private static void login(@NotNull Context ctx) throws MalformedURLException {
+       String brugernavn = ctx.queryParam("brugernavn");
+       String kodeord = ctx.queryParam("kodeord");
+       if (brugernavn==null || kodeord == null) {
+           ctx.contentType("text/html; charset=utf-8").result("<html><body><form method=get>Skriv dit fornavn: <input name=brugernavn type=text></form></html>");
+           ctx.contentType("text/html; charset=utf-8").result("<html><body><form method=get>Skriv dit fornavn: <input name=kodeord type=text></form></html>");
+       } else {
+           login.tjekLogin(brugernavn,kodeord);
+       }
+    }
 }
--- src/main/java/logik/login.java
+++ src/main/java/logik/login.java
@@ -1,4 +1,27 @@
 package logik;
 
-public class login {
+import brugerautorisation.transport.soap.Brugeradmin;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import javax.xml.ws.soap.SOAPFaultException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Login {
+
+    public boolean tjekLogin(String brugernavn, String adgangskode) throws MalformedURLException {
+        URL url = new URL("http://javabog.dk:9901/brugeradmin?wsdl");
+        QName qname = new QName("http://soap.transport.brugerautorisation/", "BrugeradminImplService");
+        Service service = Service.create(url, qname);
+
+        qname = new QName("http://soap.transport.brugerautorisation/", "BrugeradminImplPort");
+        Brugeradmin ba = service.getPort(qname, Brugeradmin.class);
+        try{
+            System.out.println(ba.hentBruger(brugernavn, adgangskode));
+            return true;
+        } catch (SOAPFaultException e) {
+            System.out.println("Forkert brugernavn eller kodeord");
+            return false;
+        }
+    }
 }
